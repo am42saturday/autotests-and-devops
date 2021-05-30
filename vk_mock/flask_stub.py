8ac8a2f1f787265ef9bec02bc80b3581be775060
Maria <am42saturday@gmail.com>
2021-05-30 23:29:03 +0300

    Initial commit (for docker-compose)

---
+++ vk_mock/flask_stub.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+SURNAME_DATA = {}
+user_id_seq = 1
+
+
+class RoutesForMock:
+
+    @staticmethod
+    @app.route('/vk_id/<name>', methods=['GET'])
+    def get_user_surname(name):
+        if name:
+            return jsonify({'vk_id': ('id' + name)}), 200
+        else:
+            return jsonify(f'ID for user {name} not found'), 404
+
+
+if __name__ == '__main__':
+    host = os.environ.get('STUB_HOST', '0.0.0.0')
+    port = os.environ.get('STUB_PORT', '8090')
+
+    app.run(host, port)
