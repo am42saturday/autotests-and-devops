e402cac9b1bb1bb0ae6c7bb3d94617ef00150388
Maria <am42saturday@gmail.com>
2021-06-01 14:31:25 +0300

    fixed pipeline script

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -6,15 +6,14 @@ pipeline {
         
         stage("Clear directory") {
             steps {
-                script {
-                    sh 'rm -rf ${WORKSPACE}/*'
-                }
+                cleanWs()
             }
         }
         
         stage('Clone docker files') {
             steps {
-                dir("${WORKSPACE}"){
+                sh 'mkdir devops'
+                dir('devops'){
                     checkout([
                         $class: 'GitSCM',
                         branches: [[name: 'master']],
@@ -25,49 +24,62 @@ pipeline {
                 }
             }
         }
-        
+
+        stage('Clone tests') {
+            steps {
+                sh 'mkdir tests'
+                dir('tests'){
+                    checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: 'master']],
+                        userRemoteConfigs: [[
+                            credentialsId: 'b562eea5-2921-42ee-8ee1-0fa3b8750da6',
+                            url: 'git@github.com:am42saturday/final_project_mail.git']]
+                    ])
+                }
+            }
+        }
+
         stage('Run and configure myapp via docker-compose') {
             steps {
                 step([
-                    $class: 'DockerComposeBuilder', 
-                    dockerComposeFile: 'docker-compose.yml', 
-                    option: [$class: 'StartAllServices'], 
+                    $class: 'DockerComposeBuilder',
+                    dockerComposeFile: 'devops/docker-compose.yml',
+                    option: [$class: 'StartAllServices'],
                     useCustomDockerComposeFile: true
                 ])
             }
         }
-        
+
         stage('Run tests') {
             steps {
-                def run_tests_command = 'cd /home/mary/Download/final_project_tests && python3 -m pytest'
-                
-                if("${MARK_TESTS}" != 'all'){
-                    run_tests_command += " -m ${MARK_TESTS}"
+                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE'){
+                    script{
+                        sh 'cd tests && python3 -m pytest ${MARK_TESTS}'
+                    }
                 }
-                
-                sh (run_tests_command)                
             }
         }
-        
+
         stage('Stop myapp') {
             steps {
                 step([
                     $class: 'DockerComposeBuilder',
-                    dockerComposeFile: 'docker-compose.yml',
+                    dockerComposeFile: 'devops/docker-compose.yml',
                     option: [$class: 'StopAllServices'],
                     useCustomDockerComposeFile: true
                 ])
             }
         }
     }
-    
+
     post {
         always{
-            dir(' /home/mary/Download/final_project_tests'){
-                allure 
-                includeProperties: false,
-                jdk: '',
-                results: [[path: 'report']]
+            dir("tests"){
+                allure ( commandline: '2.13.9',
+                    includeProperties: false,
+                    jdk: '',
+                    results: [[path: 'report']])
             }
         }
     }
